cd914786d83fef3243a16d93678a36dc6c7b9d22
wcs1010270451 <296952503@163.com>
2021-06-07 00:21:05 +0800

    新建 Article 模型

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -14,7 +14,7 @@ func ConnectDB() *gorm.DB {
 	var err error
 
 	config := mysql.New(mysql.Config{
-		DSN: "root:secret@tcp(127.0.0.1:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+		DSN: "homestead:secret@tcp(127.0.0.1:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
 	})
 
 	// 准备数据库连接池
--- tests/app/http/controllers/articles_controller.go
+++ tests/app/http/controllers/articles_controller.go
@@ -1,13 +1,15 @@
 package controllers
 
 import (
-	"database/sql"
 	"fmt"
 	"goblog/pkg/logger"
+	"goblog/pkg/model/article"
 	"goblog/pkg/route"
 	"goblog/pkg/types"
 	"net/http"
 	"text/template"
+
+	"gorm.io/gorm"
 )
 
 // ArticlesController 文章相关页面
@@ -19,10 +21,10 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 	// 1. 获取 URL 参数
 	id := route.GetRouteVariable("id", r)
 	// 2. 读取对应的文章数据
-	article, err := getArticleByID(id)
+	article, err := article.Get(id)
 	// 3. 如果出现错误
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if err == gorm.ErrRecordNotFound {
 			// 3.1 数据未找到
 			w.WriteHeader(http.StatusNotFound)
 			fmt.Fprint(w, "404 文章未找到")
