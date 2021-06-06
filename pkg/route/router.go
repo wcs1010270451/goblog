758eb548d41ed61e23f456a2adf13b643b31de5d
wcs1010270451 <296952503@163.com>
2021-06-06 23:51:03 +0800

    解决循环引用

--- pkg/route/router.go
+++ pkg/route/router.go
@@ -1,26 +1,25 @@
 package route
 
 import (
-	"goblog/tests/routes"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-var Router *mux.Router
-
-// Initialize 初始化路由
-func Initialize() {
-	Router = mux.NewRouter()
-	routes.RegisterWebRoutes(Router)
-}
-
 // RouteName2URL 通过路由名称来获取 URL
 func Name2URL(routeName string, pairs ...string) string {
-	url, err := Router.Get(routeName).URL(pairs...)
+	var route *mux.Router
+	url, err := route.Get(routeName).URL(pairs...)
 	if err != nil {
 		// checkError(err)
 		return ""
 	}
 
 	return url.String()
+}
+
+// GetRouteVariable 获取 URI 路由参数
+func GetRouteVariable(parameterName string, r *http.Request) string {
+	vars := mux.Vars(r)
+	return vars[parameterName]
 }
